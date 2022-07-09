7a142923264b1cffcc4fe9fd51c8c2219687143d
Deepakvikrant <deepakvikrant333@gmail.com>
2022-07-09 22:46:14 +0530

    Navbar Props added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       <Navbar title = "TEST_TITLE" home = "Home"/>
+      <TextForm/>
       
     </>
   );
---
+++ src/components/TextForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function TextForm() {
+  return (
+    <div>
+        <form>
+        <div class="form-group">
+            <label for="myBox">Example textarea</label>
+            <textarea class="form-control" id="myBox" rows="3"></textarea>
+        </div>
+        </form>
+    </div>
+  )
+}
